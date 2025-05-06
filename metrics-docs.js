9700537ca99d1df6991521ec84e7b1a9e705efe7
ramazantufekci <root@server>
2025-05-06 06:44:37 +0000

    kullanıcı değiştirdim

--- metrics-docs.js
+++ metrics-docs.js
@@ -1,5 +1,5 @@
 const { entries } = require("./metrics");
-
+//sadece Merak
 // DOCUMENTATION of queries and their associated metrics (targeted to DBAs)
 Object.entries(entries).forEach(([entryName, entry]) => {
   console.log("--[", entryName, "]");

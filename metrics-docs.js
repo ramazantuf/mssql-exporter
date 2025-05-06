5afc036a770acbc5cfedba70b85467ccacee53da
ramazantufekci <fabien@symfony.com>
2025-05-06 06:53:39 +0000

    kullanıcı değiştirdim

--- metrics-docs.js
+++ metrics-docs.js
@@ -1,5 +1,5 @@
 const { entries } = require("./metrics");
-//root dan burtay a Ã2denmee§iÅŸ
+//root dan symfony fabien sende gel
 // DOCUMENTATION of queries and their associated metrics (targeted to DBAs)
 Object.entries(entries).forEach(([entryName, entry]) => {
   console.log("--[", entryName, "]");
